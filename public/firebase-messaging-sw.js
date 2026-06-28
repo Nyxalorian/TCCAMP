e9eb4c291632ffa0ad38e3c7fdb87f530039c12e
Nyxalorian <moreiramaycon932@gmail.com>
2026-06-28 13:55:35 +0000

    AMP FE 0.9.7

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -15,11 +15,12 @@ const messaging = firebase.messaging();
 messaging.onBackgroundMessage((payload) => {
   console.log("Mensagem recebida:", payload);
 
-  self.registration.showNotification(
-    payload.notification?.title || "Agenda MP",
-    {
-      body: payload.notification?.body,
-      icon: "/logo192.png",
-    }
-  );
+  const title = payload.data?.title || "Agenda MP";
+  const body = payload.data?.body || "";
+
+  self.registration.showNotification(title, {
+    body: body,
+    icon: "https://pharmalife-81306.web.app/favicon.png",
+    badge: "https://pharmalife-81306.web.app/favicon.png"
+  });
 });
